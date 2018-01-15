75031c56b9b5000a351674f36f8a0e4f93cefdec
Jan Andrle <andrle.jan@centrum.cz>
2018-01-15 15:17:13 +0100

    :rocket: RC verze 0.3
    - Ošetřeno vkládání formátovaného textu (nyní vždy převeden na prostýtext)
    - Ošetření podpory napříč prohlížeči
    - Ošetření přidávání emailů, linků, obrázků
    - Doplněn README.md
    - Přidána jednoduchá ukázka HTML

--- README.md
+++ README.md
@@ -1,13 +1,29 @@
 # SimpleEditor
 Třída vytvářející s &lt;iframe>em jednoduchý editor
-## Vytvoření instance
-  ``var ja_jsem_instance= class_SimpleEditor({
+## Vytvoření editoru
+Testováno ve všech prohlížečích.
+### v JavaScriptu
+  ```javascript
+    var ja_jsem_instance= class_SimpleEditor({
     editor_element: JS_odkaz_na_iframe,
     default_value: defaultni_hodnoty
-  });``
+  });```
+### v HTML
+  ```html
+    <iframe id="editor" src='about:blank'></iframe>
+    <button onclick="ja_jsem_instance.format('bold');">Bold</button>
+  });```
 ## Metody
-  - *format*: 
-    * jako parametr bere "bold", "italic", ... 
-    * viz [Document.execCommand()](https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand)
+  - *format*(format_name): 
     * typické použití jako naslouchač na příslušné buttony
-  - *getContent*: vrací obsah <iframe>u
+    * format_name= [DOMString](https://developer.mozilla.org/en-US/docs/Web/API/DOMString) název příkazu (viz 'List příkazů' dále v textu);
+  - *getContent*: vrací HTML obsah <iframe>u
+  - *getTextContent*: vrací textový obsah obsah <iframe>u
+
+## List příkazů
+  - "bold", "italic", "underline": Přepíná bold/italic formát nově psaného (či vybraného textu)
+  - "removeFormat": zruší formátování
+  - "insertOrderedList", "insertUnorderedList", "insertParagraph": přidá ```html <ol>```, ```html <ul>```, resp. ```html <p>```
+  - "createEmail", "createLink", "insertImage": Vytvoří/zruší email, URL link či vloží obrázek buť z vybraného textu (zapsané url adresy), případně vyskočí dotaz (prompt)
+  - "justifyLeft", "justifyRight", "justifyCenter", "justifyFull": změna zarovnání
+  - Další možnosti viz [Document.execCommand()](https://developer.mozilla.org/en-US/docs/Web/API/Document/execCommand)
--- SimpleEditor.js
+++ SimpleEditor.js
@@ -1,48 +1,141 @@
 function class_SimpleEditor(def){
-    let {editor_element, default_value}= def;
+    let editor_element, default_value;
+    if(def.editor_element) editor_element= def.editor_element;
+    if(def.default_value) default_value= def.default_value;
     let _this= {};
     var editor;
+    var caret_position;
     editor_element.contentWindow.location.reload();
     editor_element.onload= function(){
         editor= editor_element.contentDocument;
-        editor.body.innerHTML= default_value;
-        editor.designMode = "on";
+        editor.body.innerHTML= default_value || "<p></p>";
+        editor.designMode= "on";
+        editor.body.addEventListener("paste", pasteHandler);
     };
-    function validateEmail(email) {
-        let re= /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        return re.test(email);
-    }
 
     _this.format= function(action){
         editor_element.contentWindow.focus();
-        let selected_value= editor.getSelection().toString();
+        let selected_value;
+        if(document.selection && document.selection.createRange){
+            selected_value= document.selection.createRange().htmlText;
+        } else if (editor.getSelection) {
+            selected_value= editor.getSelection().toString();
+        } else {
+            selected_value= '';
+        }
         let selected_value_correction= null;
         //let warning_text= "Error!";
-
-        if(action==="email"){
-            action= "createLink";
-            if(editor.getSelection().anchorNode.parentNode.nodeName!=="A"){
-                if(!validateEmail(selected_value)){
-                    selected_value= prompt("Value for email:", selected_value);
+        
+        switch (action){
+            case "createEmail":
+                action= "createLink";
+                if(getSelectionNodename()!=="A"){
                     if(!validateEmail(selected_value)){
-                        action= false;
-                        //warning_text+= " Wrong email!"
+                        selected_value= prompt("Please specify the email:", selected_value);
+                        if(!validateEmail(selected_value)){
+                            action= false;
+                            //warning_text+= " Wrong email!"
+                        } else {
+                            selected_value_correction= "mailto:"+selected_value;
+                        }
                     } else {
                         selected_value_correction= "mailto:"+selected_value;
                     }
                 } else {
-                    selected_value_correction= "mailto:"+selected_value;
+                    action= "unlink";
                 }
-            } else {
-                action= "unlink";
-            }
+                break;
+            case "createLink":
+                if(getSelectionNodename()!=="A"){
+                    if(!selected_value){
+                        selected_value= prompt("Please specify the URL link:", selected_value);
+                        if(!selected_value){
+                            action= false;
+                            //warning_text+= " Wrong email!"
+                        } else {
+                            selected_value_correction= selected_value;
+                        }
+                    } else {
+                        selected_value_correction= selected_value;
+                    }
+                } else {
+                    action= "unlink";
+                }
+                break;
+            case "insertImage":
+                if(getSelectionNodename()!=="IMG"){
+                    if(!selected_value){
+                        selected_value= prompt("Please specify the URL link of the image:", selected_value);
+                        if(!selected_value){
+                            action= false;
+                            //warning_text+= " Wrong email!"
+                        } else {
+                            selected_value_correction= selected_value;
+                        }
+                    } else {
+                        selected_value_correction= selected_value;
+                    }
+                } else {
+                    action= "unlink";
+                }
+                break;
+            default:
+                break;
         }
+        console.log(action);
         if(action) editor.execCommand(action, false, selected_value_correction);
         //else alert(warning_text);
     }
     _this.getContent= function(){
-        return editor.body.innerHTML.replace(/(<!--((?!-->).|\n)+-->)/gm,"").replace(/(<[^\/<> ]+) [^>]+(>|$)/g, "$1>");
-        //? jsete BRka?
+        return editor.body.innerHTML; //DELETE .replace(/(<!--((?!-->).|\n)+-->)/gm,"").replace(/(<[^\/<> ]+) [^>]+(>|$)/g, "$1>"); //DELETE jsete BRka?
+    }
+    _this.getTextContent= function(){
+        return editor.body.innerText; //DELETE .replace(/(<!--((?!-->).|\n)+-->)/gm,"").replace(/(<[^\/<> ]+) [^>]+(>|$)/g, "$1>"); //DELETE jsete BRka?
+    }
+
+    function validateEmail(email) {
+        let re= /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+        return re.test(email);
+    }
+    function pasteHandler(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        const pastedData= (e.clipboardData || window.clipboardData).getData('Text');
+        
+        let sel, range;
+        if(editor.getSelection){
+            sel= editor.getSelection();
+            if (sel.getRangeAt && sel.rangeCount) {
+                range= sel.getRangeAt(0);
+                range.deleteContents();
+                range.insertNode(editor.createTextNode(pastedData));
+            }
+        } else if(document.selection && document.selection.type !== "Control"){// IE < 9
+            document.selection.createRange().pasteHTML(pastedData);
+        }
+    }
+    function getSelectionNodename(){
+      var range;
+      if(document.selection && document.selection.createRange){
+        range = document.selection.createRange();
+        return range.htmlText;
+      }
+      else if (editor.getSelection) {
+        var selection= editor.getSelection();
+        if(selection.anchorNode.nodeName!=="#text"){
+          range= selection.getRangeAt(0);
+          var clonedSelection=range.cloneContents();
+          var div= document.createElement('div');
+          div.appendChild(clonedSelection);
+          return div.childNodes[0].nodeName;
+        }
+        else {
+          return editor.getSelection().anchorNode.parentNode.nodeName;
+        }
+      }
+      else {
+        return '';
+      }
     }
     return Object.freeze(_this);
 }
